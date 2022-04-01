0e73f0ad966cfc4c0041dbb0db2cb6a102afd5e8
MaksimAren <92427582+MaksimAren@users.noreply.github.com>
2022-04-01 20:07:14 +0700

    Add files via upload

---
+++ ARRAYINT.h
@@ -0,0 +1,102 @@
+#ifndef ARRAYINT_h
+#define ARRAYINT_h
+
+#include <cassert>
+
+class ArrayInt
+{
+private:
+    int m_length;
+    int *m_data;
+
+public:
+    ArrayInt():
+        m_length(0), m_data(nullptr)
+    {
+    }
+    
+    ArrayInt(int length):
+        m_length(length)
+    {
+            assert(length >= 0);
+            
+            if (length > 0)
+                m_data = new int[length];
+            else
+                m_data = nullptr;
+    }
+    
+    ~ArrayInt()
+    {
+        delete[] m_data;
+    }
+    
+    void erase()
+    {
+        delete[] m_data;
+        
+        m_data = nullptr;
+        m_length = 0;
+    }
+    
+    int& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length);
+        return m_data[index];
+    }
+    
+    const int& operator[](int index) const
+    {
+        assert(index >= 0 && index < m_length);
+        return m_data[index];
+    }
+    
+    void insertBefore(int value, int index)
+    {
+        assert(index >= 0 && index <= m_length);
+        
+        int *data = new int[m_length + 1];
+        
+        for (int before = 0; before < index; ++before)
+            data[before] = m_data[before];
+        
+        data[index] = value;
+        
+        for (int after = index; after < m_length; ++after)
+            data[after + 1] = m_data[after];
+        
+        delete[] m_data;
+        m_data = data;
+        ++ m_length;
+    }
+    
+    void remove(int index)
+    {
+        assert(index >= 0 && index <= m_length);
+        
+        if (m_length == 1)
+        {
+            erase();
+            return;
+        }
+        
+        int *data = new int[m_length - 1];
+        
+        for (int before = 0; before < index; ++before)
+            data[before] = m_data[before];
+        
+        for (int after = index + 1; after < m_length; ++after)
+            data[after - 1] = m_data[after];
+        
+        delete[] m_data;
+        m_data = data;
+        -- m_length;
+    }
+    
+    void insertAtBeginning(int value) {insertBefore(value, 0);}
+    void insertAtEnd(int value) {insertBefore(value, m_length);}
+    
+    int getLength() {return m_length;}
+};
+    
+#endif
---
+++ Container.h
@@ -0,0 +1,142 @@
+#ifndef Container_h
+#define Container_h
+
+#include <cassert>
+
+template <class T>
+class Container
+{
+public:
+    Container():
+        m_length(0), m_data(nullptr)
+    {
+    }
+    
+    Container(int length):
+        m_length(length)
+    {
+        assert(length >= 0);
+        
+        if (length > 0)
+            m_data = new T[length];
+        else
+            m_data = nullptr;
+    }
+    
+    ~Container()
+    {
+        delete[] m_data;
+    }
+    
+    void insert(T value, int where)
+    {
+        assert(where >= 0 && where <= m_length);
+        
+        int *data = new int[m_length + 1];
+        
+        for (int before = 0; before < where; ++before)
+            data[before] = m_data[before];
+        
+        data[where] = value;
+        
+        for (int after = where; after < m_length; ++after)
+            data[after + 1] = m_data[after];
+        
+        delete[] m_data;
+        m_data = data;
+        ++ m_length;
+    }
+    
+    void insertAtBeginning(T value);
+    
+    void insertAtEnd(T value);
+    
+    void erase(int where)
+    {
+        assert(where >= 0 && where < m_length);
+        
+        if (m_length == 1)
+        {
+            clear();
+            return;
+        }
+        
+        int *data = new int[m_length - 1];
+        
+        for (int before = 0; before < where; ++before)
+            data[before] = m_data[before];
+        
+        for (int after = where + 1; after < m_length; ++after)
+            data[after - 1] = m_data[after];
+        
+        delete[] m_data;
+        m_data = data;
+        -- m_length;
+    }
+    
+    void eraseAtBeginning();
+    
+    void eraseAtEnd();
+    
+    T& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length);
+        
+        return m_data[index];
+    }
+    
+    int getLength()
+    {
+        return m_length;
+    }
+    
+    bool isEmpty()
+    {
+        return (getLength() == 0);
+    }
+    
+    void swap(Container<T>& container) //реализовать
+    {
+        std::cout << "swapTEST length = " << container.getLength() << "\n";
+    }
+    
+    void reverse()
+    {
+        if (m_length > 1)
+        {
+            int *data = new int[m_length];
+            for (int i = 0; i < m_length; ++i)
+                data[i] = m_data[m_length - 1 - i];
+            
+            delete[] m_data;
+            m_data = data;
+        }
+    }
+    
+    void clear()
+    {
+        delete[] m_data;
+        
+        m_data = nullptr;
+        m_length = 0;
+    }
+    
+private:
+    int m_length;
+    T *m_data;
+ 
+};
+
+template <typename T>
+void Container<T>::insertAtBeginning(T value) { insert(value, 0); }
+
+template <typename T>
+void Container<T>::insertAtEnd(T value) { insert(value, m_length); }
+
+template <typename T>
+void Container<T>::eraseAtBeginning() { erase(0); }
+
+template <typename T>
+void Container<T>::eraseAtEnd() { erase(m_length - 1); }
+
+#endif
